89584475594ca8f8e0b3702abaef13e0db6a5c75
ManbirJaspal <manb.jaspal@gmail.com>
2017-08-12 12:18:31 -0400

    starting
    php

---
+++ variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+<title></title>    
+    
+</head>
+    <body>
+        <h1></h1>
+
+<?php
+        $name = "Manbir";
+        echo $name;
+        
+        $color = "red";
+        echo"<br>I love $color color";
+        ?>
+    </body>
+</html>
